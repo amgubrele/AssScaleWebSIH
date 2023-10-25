8d1d7f9b897208e4e1e3975a3ef083f505fe7483
amgubrele <amangubrele011@gmail.com>
2023-10-25 12:11:09 +0530

    home page

--- server/home-page/logic.js
+++ server/home-page/logic.js
@@ -6,12 +6,15 @@ const toggleNavbar=()=>{
     nav_header.classList.toggle('active')
     if(f==1)
     {
+        section1.style.display="none"
         document.querySelector('.body').style.overflowY= "hidden";
         f=0;
-    }
+    }//kljda
     else if(f==0)
     {
+        section1.style.display="flex"
         document.querySelector('.body').style.overflowY= "scroll";
+        f=1;
     }
 
 }
