f306dc8527c3398fe2625e1d7a208066cd9dbe2e
amgubrele <amangubrele011@gmail.com>
2023-10-29 14:27:00 +0530

    search

--- server/datajs/books.js
+++ server/datajs/books.js
@@ -1,20 +1,24 @@
 let book=[
     {
-        name:"book1",
-        author:"aman",
-        des:"book about aman",
+        name:"ayurveda",
+        author:"DR. U.R. Sekhar Namburi",
+        des:"text book of Vyavaharayurveda and Vidhivaidhyka",
         price:"450rs",
         isRated:false,
-        rating:false
+        rating:false,
+        publisher:"Foresic Medical",
+        link:"https://m.media-amazon.com/images/I/81ctRJFn2DL._AC_UF1000,1000_QL80_.jpg"
 
     },
     {
-        name:"book2",
-        author:"aman",
+        name:"health management",
+        author:"Dr. Mohapatra ",
         des:"book about aditya",
         price:"45rs",
         isRated:false,
-        rating:false
+        rating:false,
+        publisher:"IP-Innovative",
+        link:"https://www.ipinnovative.com/media/books/Mohapatra_Cover.jpg"
 
     },
     {
